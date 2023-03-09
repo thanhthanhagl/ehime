73ba05761c5cf47488fa94a5dfb24151619a696a
thanhthanhagl <tran_diep_thanh_thanh@allgrow-labo.jp>
2023-03-09 18:06:56 +0700

    update

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -1,1 +1,52 @@
-console.log("main.js");
+jQuery(function ($) {
+    //
+    // 変数を定義
+    //------------------------------------
+    var $body = $('body'),
+        $header = $('.c-header'),
+        $menuButton = $('.c-menu'),
+        desktopMode = ($menuButton.css('display') != 'none');
+    function headerHeight() {
+        $headerHeight = $header.outerHeight();
+    }
+    headerHeight();
+
+    //
+    // header-menu
+    //------------------------------------
+    //ハンバーガーボタンクリック
+    $menuButton.click(function () {
+        if ($(this).hasClass('is-open')) {
+            $(this).removeClass('is-open');
+            $('.c-header__item.is-hover').removeClass('is-open');
+        } else {
+            $(this).addClass('is-open');
+        }
+    });
+
+    //SPメニュー内アコーディオン
+    $(".c-header__item.is-hover").click(function (e) {
+        if (!desktopMode) {
+            if ($(this).hasClass('is-open')) {
+                $(this).removeClass('is-open');
+            } else {
+                $(this).addClass('is-open');
+            }
+        }
+    });
+    //
+    // pagetop
+    //------------------------------------
+    var $pagetop = $('.js-pagetop');
+    $pagetop.click(function () {
+        $('body,html').animate({ scrollTop: 0 }, 500);
+        return false;
+    });
+    //
+    //filter post category
+    //------------------------------------
+    $('.c-list__catitem').click(function () {
+        $('.c-list__catitem').removeClass('is-active');
+        $(this).addClass('is-active');
+    });
+});
